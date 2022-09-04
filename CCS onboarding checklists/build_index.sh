68ed7a2a9cd55ba822656d4382bc515b644ddb1d
Avital Pinnick <apinnick@redhat.com>
2022-09-04 12:40:52 +0300

    updates for CCS onboarding checklists

--- CCS onboarding checklists/build_index.sh
+++ CCS onboarding checklists/build_index.sh
@@ -5,7 +5,7 @@
 
 BUILD_URL=https://ccs-internal-documentation.pages.redhat.com/onboarding-checklists
 HTML=html_files
-DOCX=docx_files
+ODT=odt_files
 
 rm index.md > /dev/null 2>&1
 
@@ -14,20 +14,28 @@ cat << EOF > index.md
 layout: home
 ---
 
-Onboarding checklists:
+Welcome to the official site for CCS onboarding checklists.
+
+These checklists are updated regularly and have been approved by the [CCS Onboarding Program team](https://source.redhat.com/groups/public/ccs-onboarding-program).
+
+You can view the checklists in a browser or download them as ODT files and import them into Google docs.
+
+Onboarding checklists (HTML):
 EOF
 
 for h in $HTML/*; do
   echo -e "- [$(basename $h | sed 's/.html//g; s/_/ /g; s/[a-z]*/\u&/g' )]($BUILD_URL/$HTML/$(basename $h))" >> index.md
 done
 
-echo -e "\nDOCX files to import into Google docs:" >> index.md
+echo -e "\nODT files:" >> index.md
 
-for d in $DOCX/*; do
-  echo -e "- [$(basename $d)]($BUILD_URL/$DOCX/$(basename $d))" >> index.md
+for o in $ODT/*; do
+  echo -e "- [$(basename $o)]($BUILD_URL/$ODT/$(basename $o))" >> index.md
 done
 
 echo -e "\nIf you see something that needs to be fixed, you can submit a [GitLab issue](https://gitlab.cee.redhat.com/ccs-internal-documentation/onboarding-checklists/-/issues/new?issue%5Bmilestone_id%5D=)." >> index.md
+
+echo -e "\nSite maintainers: [Avital Pinnick](mailto:apinnick@redhat.com), [Jiri Herrmann](mailto:jherrman@redhat.com)" >> index.md
 
 echo -e "\n_Files generated on $(date +%b\ %-e\ %Y -r $h)_" >> index.md
 
--- CCS onboarding checklists/build_odt.sh
+++ CCS onboarding checklists/build_odt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Avital Pinnick, 2021
-# Builds html and docx files
+# Builds html and odt files
+# This is for testing. Actual build is done in GitLab CI.
 
 SOURCE=html_files
-DEST=docx_files
+DEST=odt_files
 
 # delete current build
 rm -fr $SOURCE
@@ -13,10 +14,10 @@ rm -fr $DEST
 ./build_tools/build_html.sh
 
 # build DOCX files
-echo "Converting HTML to DOCX:"
+echo "Converting HTML to ODT:"
 
 mkdir $DEST
 for f in $SOURCE/*.html; do
-  echo "- $(basename "${f%%.*}").docx"
-  pandoc -o $DEST/$(basename "${f%%.*}").docx $f
+  echo "- $(basename "${f%%.*}").odt"
+  pandoc -o $DEST/$(basename "${f%%.*}").odt $f
 done

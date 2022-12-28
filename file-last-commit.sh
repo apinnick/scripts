c0f81b8d33047d4787a5d82a710032280f88b339
Avital Pinnick <apinnick@redhat.com>
2022-12-28 11:49:18 +0200

    update

--- file-last-commit.sh
+++ file-last-commit.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Avital Pinnick, December 8, 2022
-# This script shows the last commit for each file in a directory.
-# Use it to detect changes made to files since your last commit.
-# Usage
-# Example: $ ./file-last-commit.sh <target_directory>
+# Avital Pinnick, December 28, 2022
+# This script generates a file containing a list of the last commit for each file in a directory.
+# Usage: $ ./file-last-commit.sh <target_directory>
 
 if [ -z "$1" ];
 then
@@ -12,16 +10,16 @@ then
 fi
 
 DIR=$1
-LAST_COMMIT=last_commit.txt
+LAST_COMMIT=file-last-commit.txt
 
-echo -e "\nMost recent Git commit for files in $DIR:\n" > $LAST_COMMIT
+echo -e "Most recent Git commit for files in $DIR:\n" > $LAST_COMMIT
 
 for file in $DIR/*; do
   echo -e "$(basename $file):" >> $LAST_COMMIT
   # '-n' sets the number of commits to display.
-  git -C "$DIR" log -n 1 --pretty=format:"- %an, %ad %h: %s %n" $(basename $file) >> $LAST_COMMIT
+  git -C "$DIR" log -n 1 --date=short --pretty=format:"  %cn %cd (%cr)%n  %h: %s %n" $(basename $file) >> $LAST_COMMIT
   echo "" >> $LAST_COMMIT
 done
 
-echo -e "Generated '$LAST_COMMIT' with most recent Git commit for files in '$DIR'."
+echo -e "\nGenerated './$LAST_COMMIT' with most recent Git commit for files in '$DIR'."
 echo Done

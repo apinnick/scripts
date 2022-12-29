386f426cb97adf37ed56411529874642a304cd52
Avital Pinnick <apinnick@redhat.com>
2022-12-29 16:31:46 +0200

    Added file string variable to script

--- file-last-commit.sh
+++ file-last-commit.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-# Avital Pinnick, December 28, 2022
-# This script generates a file containing a list of the last commit for each file in a directory.
-# Usage: $ ./file-last-commit.sh <target_directory>
+# Avital Pinnick, December 29, 2022
+# This script generates a list of files, showing the last commit date.
+# Useful for identifying files that might be abandoned or obsolete.
+# Usage: $ ./file-last-commit.sh </directory> OPT:<file_name_string>
 
 if [ -z "$1" ];
 then
-  printf 'No directory specified.\n\nUsage: $ ./file-last-commit.sh <target_directory>\n\nExiting...\n\n'
+  printf 'You must specify a directory. Optional: File name string.\n\nExamples:\n$ ./file-last-commit.sh ../openshift-docs/modules virt- (for module names containing "virt-")\n$ ./file-last-commit.sh ../openshift-docs/virt/install (all files)\n\nExiting...\n\n'
   exit 2
 fi
 
+if [ -z "$2" ];
+then
+  STRING=*
+fi
+
 DIR=$1
+STRING=$2
+TOTAL=$(ls -1 $DIR/*$STRING*.adoc | wc -l)
 LAST_COMMIT=file-last-commit.txt
 
-echo -e "Most recent Git commit for files in $DIR:\n" > $LAST_COMMIT
+echo -e "Last Git commit for $TOTAL files containing '$STRING' in $DIR:\n" > $LAST_COMMIT
 
-for file in $DIR/*; do
-  echo -e "$(basename $file):" >> $LAST_COMMIT
-  # '-n' sets the number of commits to display.
-  git -C "$DIR" log -n 1 --date=short --pretty=format:"  %cn %cd (%cr)%n  %h: %s %n" $(basename $file) >> $LAST_COMMIT
-  echo "" >> $LAST_COMMIT
+for file in $DIR/*$STRING*.adoc; do
+    echo -e $(basename $file) >> $LAST_COMMIT
+    # '-n' sets the number of commits to display.
+    git -C "$DIR" log -n 1 --date=short --pretty=format:"%cr: %cn %cd, %h %s %n" $(basename $file) >> $LAST_COMMIT
+    echo "" >> $LAST_COMMIT
 done
 
-echo -e "\nGenerated './$LAST_COMMIT' with most recent Git commit for files in '$DIR'."
+echo -e "\nGenerated './$LAST_COMMIT' with last commit for $TOTAL files containing '$STRING' in '$DIR'."
 echo Done

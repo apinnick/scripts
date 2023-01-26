5663eb5cc0957506ba6ca3e96653f9334eb6a742
Avital Pinnick <apinnick@redhat.com>
2023-01-26 16:27:38 +0200

    refinements

--- errata-build-list.sh
+++ errata-build-list.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # Avital Pinnick, Jan. 25, 2023
-# This script generates a list of package names from the Build tab of the errata tool
-# Copy the contents of the builds tab and save as a text file.
+# This script generates a list of package names from text copied from the Build tab of the Errata Tool.
+# Copy the contents of the Builds tab to a text file and run the script.
 #
 # Usage: $ ./errata-build-list.sh <build.txt>
 
 if [ -z "$1" ];
 then
-  printf '\nYou must specify a build file.\n\nExiting...\n\n'
+  printf '\nYou must specify a target file.\n\nExiting...\n\n'
   exit 2
 fi
 
 OLD_LIST=$1
-NEW_LIST=new-build-list.txt
+NEW_LIST=clean-package-list.txt
 
+rm $NEW_LIST &>/dev/null
 cp $OLD_LIST ./$NEW_LIST
 
-sed -i 's/^Product.*//g; s/^Non-RPM.*//g; s/^tar.*//g; s/^unsigned.*//g' $NEW_LIST
+# Remove unwanted text
+sed -i 's/^Product.*//g; s/^Non.*//g; s/^tar.*//g; s/^unsigned.*//g; s/^Remove.*//g' $NEW_LIST
+# Remove blank lines
 sed -i '/^$/d' $NEW_LIST
+# Sort list alphabetically
 sort -o $NEW_LIST{,}
 
-echo -e "Generated $NEW_LIST"
-
+echo -e "\nGenerated ./$NEW_LIST"
 echo -e "Done\n"

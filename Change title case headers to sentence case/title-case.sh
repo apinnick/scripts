af3f50ce65de2c80d7bd72cbe986c902d69ee319
Avital Pinnick <apinnick@redhat.com>
2024-04-01 14:48:43 +0200

    Update title case script - Added progress messages - Added 'exclude' terms

--- Change title case headers to sentence case/title-case.sh
+++ Change title case headers to sentence case/title-case.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-# Avital Pinnick, March 28, 2024
+# Avital Pinnick, April 1, 2024
 # Script to change "title case" headers and captions to "sentence case".
-# I recommend trying this on a small number of files in case the results are unexpected.
 
+# I recommend trying this on a small number of files in case the results are unexpected.
 # How to use this script:
 if [ -z "$1" ];
 then
@@ -13,37 +12,51 @@ fi
 
 if [ -z "$2" ];
 then
-  STRING=""
+  STRING=*
 fi
 
 DIR=$1
 STRING=$2
 
-# Changes headers to sentence case: "= My Header" to "= My header","= My Sub-Section" to "= My sub-section"
-HEADERS='/^=/s/\([A-Za-z0-9}]\) \([A-Z][a-z]\)/\1 \L\2/g;/^=/s/\([A-Za-z0-9}]\)-\([A-Z]\)/\1-\L\2/g'
-# Changes captions to sentence case: ".My Caption" to ".My caption",".My Sub-Section" to ".My sub-section"
+echo $DIR
+echo $STRING
+
+# Changes single and hyphenated words in headers to sentence case.
+# Examples: "= My Header" to "= My header","= My Sub-Section" to "= My sub-section"
+HEADERS='/^=/s/\([A-Za-z0-9}]\) \([A-Z][a-z]\)/\1 \L\2/g;/^=/s/\([A-Za-z0-9}]\)-\([A-Z][a-z]\)/\1-\L\2/g'
+
+# Changes single and hyphenated words in captions to sentence case.
+# Examples: ".My Caption" to ".My caption",".My Sub-Section" to ".My sub-section"
 CAPTIONS='/^\.[A-Za-z0-9}]/s/\([A-Za-z0-9}]\) \([A-Z][a-z]\)/\1 \L\2/g;/^\.[A-Za-z0-9}]/s/\([A-Za-z0-9}]\)-\([A-Z][a-z]\)/\1-\L\2/g'
 
-# Single names that you want to exclude are defined in "exclude.txt".
+# Excluding terms:
+
+# Single-word names are defined in "exclude.txt". The first letter of the name must be lower case: "ansible","iPv6", etc.
 # The "exclude.txt" file is mapped to the EXCLUDE array.
 mapfile -t EXCLUDE < <(sed 's/^\([A-Za-z]\)/\l\1/g' exclude.txt)
 
-# Compound names that you want to exclude are defined in "complex_lc" array and replaced by "complex_uc" array.
+# Compound names are defined in "complex_lc" array and replaced by "complex_uc" array.
+# They are listed here because reading them from a file does not work if string contains spaces.
 # This solution is not very pretty, so I might try to find something better.
-complex_lc=("[Cc]entOS stream" "[Ss]alt minion" "[Aa]mazon web service" "[Aa]zure resource manager" \
-  "[Pp]uppet agent" "[Ii]nsights inventory" "[Hh]at cloud" "[Tt]ransparent hugepage" "release files"\
-  "[Cc]ontent delivery network" "[Hh]at identity management" "[Hh]ybrid cloud console" "[Aa]ctive directory")
+complex_lc=("[Cc]entOS stream" "[Ss]alt minion" "[Aa]zure resource manager" \
+  "[Ii]nsights inventory" "[Hh]at cloud" "[Tt]ransparent hugepage" "release files" \
+  "[Cc]ontent delivery network" "[Hh]at identity management" "[Hh]ybrid cloud console" \
+  "[Aa]ctive directory" "[Rr]ed [Hh]at" "[Aa]mazon web service" "[Gg]oogle compute engine" \
+  "[Ss]alt client" "[Ss]alt master" "[Aa]nsible collections" "[Aa]nsible vault" )
 
-complex_uc=("CentOS Stream" "Salt Minion" "Amazon Web Service" "Azure Resource Manager" \
-  "Puppet Agent" "Insights Inventory" "Hat Cloud" "Transparent Hugepage" "Release files" \
-  "Content Delivery Network" "Hat Identity Management" "Hybrid Cloud Console" "Active Directory")
+complex_uc=("CentOS Stream" "Salt Minion" "Azure Resource Manager" \
+  "Insights Inventory" "Hat Cloud" "Transparent Hugepage" "Release files" \
+  "Content Delivery Network" "Hat Identity Management" "Hybrid Cloud Console" \
+  "Active Directory" "Red Hat" "Amazon Web Service" "Google Compute Engine" \
+  "Salt Client" "Salt Master" "Ansible Collections" "Ansible Vault" )
 
-for file in $DIR$STRING; do
+for file in $DIR/$STRING*; do
+  echo Checking $file
   sed -i -e "$HEADERS" $file
   sed -i -e "$CAPTIONS" $file
   # Fix names on the "exclude.txt" list
   for name in ${EXCLUDE[@]}; do
-    sed -i -e "/^=/s/\b\($name\)/\u\0/g;/^\.[A-Za-z0-9}]/s/\b\($name\)/\u\0/g" $file
+    sed -i -e "/^=/s/\($name\)/\u\0/g;/^\.[A-Za-z0-9}]/s/\($name\)/\u\0/g" $file
   done
   # Fix compound names
   for index in "${!complex_lc[@]}"; do
@@ -51,3 +64,4 @@ for file in $DIR$STRING; do
   done
 done
 
+echo Done
